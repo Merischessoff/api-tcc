c93dd7b82a83988bc3e4a0cb10e0527a5494399f
Merischessoff <gonkaschessoff@gmail.com>
2023-05-07 13:00:49 -0300

    associar e desassociar usuario banco historias

--- src/main/java/tcc/histsoc/api/controller/BancoDeHistoriaSocialController.java
+++ src/main/java/tcc/histsoc/api/controller/BancoDeHistoriaSocialController.java
@@ -21,7 +21,7 @@ public class BancoDeHistoriaSocialController {
     private BancoDeHistoriaSocialRepository repositoryHistSoc;
 
     
-    @GetMapping
+    @GetMapping("/pesquisa")
     public ResponseEntity<List<DadosListagemBancoDeHistoriaSocial>> listarBancoDeHistoriasSociais() {
         List<BancoDeHistoriaSocial> historias = repositoryHistSoc.findAll();
         List<DadosListagemBancoDeHistoriaSocial> lista = historias.stream()
@@ -29,7 +29,7 @@ public class BancoDeHistoriaSocialController {
         return ResponseEntity.ok(lista);
     }
 
-    @GetMapping("/pesquisa/{emailleitor}")
+    @GetMapping("/pesquisa/associado/{emailleitor}")
     public ResponseEntity<List<DadosListagemBancoDeHistoriaSocial>> listarHistoriasPropriasPorEmailUsuarioAssociado(@PathVariable String emailleitor) {
         List<BancoDeHistoriaSocial> historias = repositoryHistSoc.findByAllEmailBancoDeHistoriaUsuarioAssociado(emailleitor);
         List<DadosListagemBancoDeHistoriaSocial> lista = historias.stream()
@@ -38,14 +38,23 @@ public class BancoDeHistoriaSocialController {
         return ResponseEntity.ok(lista);
     }
 
-    @PostMapping("/{idbancodehistoria}/{idusuario}")
+    @GetMapping("/pesquisa/desassociado/{emailleitor}")
+    public ResponseEntity<List<DadosListagemBancoDeHistoriaSocial>> listarHistoriasPropriasPorEmailUsuarioDesassociado(@PathVariable String emailleitor) {
+        List<BancoDeHistoriaSocial> historias = repositoryHistSoc.findByAllEmailBancoDeHistoriaUsuarioAssociado(emailleitor);
+        List<DadosListagemBancoDeHistoriaSocial> lista = historias.stream()
+                                               .map(historia -> new DadosListagemBancoDeHistoriaSocial(historia))
+                                               .collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
+    }
+
+    @PostMapping("/associar/{idbancodehistoria}/{idusuario}")
     @Transactional
     public ResponseEntity<String> vincularBancoDeHistoriaSocial(@PathVariable Long idbancodehistoria, @PathVariable Long idusuario) {
         if (idbancodehistoria == null || idusuario == null) {
             return ResponseEntity.badRequest().body("Os parâmetros idbancodehistoria e idusuario são obrigatórios.");
         }
     
-        repositoryHistSoc.linkUsuarioBancoDeHistorias(idusuario, idbancodehistoria);
+        repositoryHistSoc.associarUsuarioBancoDeHistorias(idusuario, idbancodehistoria);
         return ResponseEntity.ok("A vinculação foi realizada com sucesso.");
     }
 
--- src/main/java/tcc/histsoc/api/repository/BancoDeHistoriaSocialRepository.java
+++ src/main/java/tcc/histsoc/api/repository/BancoDeHistoriaSocialRepository.java
@@ -13,7 +13,14 @@ public interface BancoDeHistoriaSocialRepository extends JpaRepository<BancoDeHi
     @Query(value = "SELECT * FROM bancodehistoria bh WHERE bh.id NOT IN (SELECT IFNULL(hul.banco_de_historia_id, 0) FROM usuario u LEFT OUTER JOIN bancodehistoria_usuarios_leitores hul on u.id = hul.usuarios_leitores_id WHERE u.email = ?1)", nativeQuery = true)
     List<BancoDeHistoriaSocial> findByAllEmailBancoDeHistoriaUsuarioAssociado(String emailLeitor);
 
+    @Query(value = "SELECT * FROM bancodehistoria bh WHERE bh.id IN (SELECT IFNULL(hul.banco_de_historia_id, 0) FROM usuario u LEFT OUTER JOIN bancodehistoria_usuarios_leitores hul on u.id = hul.usuarios_leitores_id WHERE u.email = ?1)", nativeQuery = true)
+    List<BancoDeHistoriaSocial> findByAllEmailBancoDeHistoriaUsuarioDesassociado(String emailLeitor);
+
     @Modifying
     @Query(value = "INSERT INTO bancodehistoria_usuarios_leitores (usuarios_leitores_id, banco_de_historia_id) VALUES(?1, ?2) ", nativeQuery = true)
-    void linkUsuarioBancoDeHistorias(Long idUsuario, Long idBancoDeHistoria);
+    void associarUsuarioBancoDeHistorias(Long idUsuario, Long idBancoDeHistoria);
+
+    @Modifying
+    @Query(value = "DELETE bancodehistoria_usuarios_leitores WHERE usuarios_leitores_id = ?1 AND banco_de_historia_id = ?2", nativeQuery = true)
+    void desassociarUsuarioBancoDeHistorias(Long idUsuario, Long idBancoDeHistoria);
 }
